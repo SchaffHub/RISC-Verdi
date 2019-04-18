35ab33283c741095fe4201c2875c4d28b4515c84
Mike Schaffstein <mike@dovermicrosystems.com>
2019-04-18 15:56:02 -0400

    Scrubbed clean, go for open source.

--- sw/host.c
+++ sw/host.c
@@ -30,108 +30,101 @@ typedef unsigned long uint32_t;
 volatile uint32_t *const outputPort    = (uint32_t *)(0x3FFFF8);
 volatile uint32_t *const endSimulation = (uint32_t *)(0x3FFFFC);
 
-char pi100[] = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+char license[] = 
+    "Permission is hereby granted, free of charge, to any person "
+    "obtaining a copy of this software and associated documentation "
+    "files (the \"Software\"), to deal in the Software without restriction, "
+    "including without limitation the rights to use, copy, modify, merge, "
+    "publish, distribute, sublicense, and/or sell copies of the Software, "
+    "and to permit persons to whom the Software is furnished to do so, "
+    "subject to the following conditions: "
+    "The above copyright notice and this permission notice shall be "
+    "included in all copies or substantial portions of the Software.  "
+    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, "
+    "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF "
+    "MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND "
+    "NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT "
+    "HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, "
+    "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
+    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER "
+    "DEALINGS IN THE SOFTWARE.";
 
-double pi(int iterations) {
-    // Implement Gregory-Leibniz series.
+bool isCharInWord(char ch) {
+    return (bool)(((ch >= 'a') & (ch <= 'z')) | ((ch >= 'A') & (ch <= 'Z')));
+}
 
-    double approxPi = 0;
-    bool add = true;
+int countWords(char *text) {
+    int numWords = 0;
+    bool inWord = false;
 
-    for (int i = 0; i < iterations; i++) {
-        if (add) {
-            approxPi += 4.0 / (double)(i*2 + 1);
-        } else {
-            approxPi -= 4.0 / (double)(i*2 + 1);
+    for (; *text; text++) {
+        if (inWord && !isCharInWord(*text)) {
+            numWords++;
+            inWord = false;
+        } else if (!inWord && isCharInWord(*text)) {
+            inWord = true;
         }
-        add != add;
     }
-    return approxPi;
+    if (inWord) {
+        numWords++;
+    }
+    return numWords;
 }
 
-void stringToOutputPort(char *str) {
-    for (; *str; str++) {
-        *outputPort = (uint32_t)(*str);
+void printWord(char *text) {
+    for (; *text & (*text != ' '); text++) {
+        *outputPort = (uint32_t)(*text);
     }
 }
 
-char *addCharToString(char *str, int *length, char ch) {
-    if (*length > 0) {
-        *str = ch;
-        str++;
-        *length--;
+void printString(char *text) {
+    for (; *text; text++) {
+        *outputPort = (uint32_t)(*text);
     }
-    return str;
 }
 
-void doubleToString(double num, char *str, int length) {
+void printInt(int num) {
 
-    // Start with the integer portion which we'll limit
-    // to the max value of an int.
-    int intNum = (int)num;
-    int digits = 0;
+    // Determine the number of digits.
+    int digits  = 1;
     int divisor = 1;
-
-    // Determine the log base 10 of the int portion.
-    for (int loopNum = intNum; loopNum > 0; loopNum /= 10) {
-        digits++;
+    for (int tmpNum = num; tmpNum >= 10; digits++) {
+        tmpNum /= 10;
         divisor *= 10;
     }
 
-    // Now stream out the int portion.
-    for (int loopNum = intNum; digits > 0; digits--) {
-        char ch = (char)(loopNum / divisor) + '0';
-        str = addCharToString(str, &length, ch);
-        loopNum %= divisor;
+    // Now stream out by most significant digit.
+    for (int tmpNum = num; digits > 0; digits--) {
+        char ch = (char)(tmpNum / divisor) + '0';
+        *outputPort = (uint32_t)(ch);
+        tmpNum %= divisor;
         divisor /= 10;
     }
-
-    // Decimal point.
-    str = addCharToString(str, &length, '.');
-
-    // Now the fractional part.  This is going to
-    // accumulate error.
-    double fraction = num - (double)intNum;
-    while (length > 1) {
-        fraction *= 10;
-        char digit = (char)fraction;
-        char ch = digit + '0';
-        str = addCharToString(str, &length, ch);
-        fraction -= (double)digit;
-    }
-
-    // Terminate the string.
-    str = addCharToString(str, &length, '\0');
 }
 
-int fib(int iterations) {
-    int prev   = 0;
-    int result = 1;
-    switch (iterations) {
-    case 1: result = prev; break;
-    case 2: result = 1;    break;
-    default:
-        for (int i = 3; i < iterations; i++) {
-            int tmp = result;
-            result += prev;
-            prev = tmp;
+int printFibonacci(int count) {
+    int f[2] = { 1, 1 };
+    for (int i = 0; i < count; i++) {
+        if (i < 2) {
+            printInt(f[i]);
+        } else {
+            int newF = f[1] + f[0];
+            printInt(newF);
+            f[0] = f[1];
+            f[1] = newF;
+        }
+        if (i != (count-1)) {
+            printString(",");
         }
     }
-    return result;
 }
 
 void main() {
-    double approxPiDouble = pi(5);
-
-    const int bufferSize = 10;
-    char approxPiStr[bufferSize];
-
-    //doubleToString(approxPiDouble, approxPiStr, bufferSize);
-
-    //stringToOutputPort(approxPiStr);
-
-    char hello[] = "Hello World!";
-    stringToOutputPort(hello);
-    //int fib_num = fib(10);
+    int numWords = countWords(license);
+    printString("Number of words in license: ");
+    printInt(numWords);
+    printString("; First 10 Fibonacci numbers are: ");
+    printFibonacci(10);
+    printString("; Test complete.");
     *endSimulation = 0;
 }
